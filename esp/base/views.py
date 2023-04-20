0ec61f8c13d3a1e23e52fbdedfe30571fb3cce4c
root <root@ip-172-31-8-175.ap-south-1.compute.internal>
2023-04-20 00:29:17 +0000

    not sig fig

--- esp/base/views.py
+++ esp/base/views.py
@@ -15,7 +15,7 @@ def home(request):
         data = str(request.POST)
         position = data.find(']}>')
         value = int(data[position-2:position-1])
-        output = esp.objects.filter(id=1).update(state=value)
+        esp.objects.filter(id=1).update(state=value)
         print(value)
 
         return render(request, "index.html", {'val': value})
